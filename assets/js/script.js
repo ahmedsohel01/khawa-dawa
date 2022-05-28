f26197690128ce81e021e0c671f901888e9c319c
ahmedsoahl01 <sohel609.bd@gmail.com>
2022-05-28 21:30:14 +0600

    Moboile Done For Home Page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,37 @@
 jQuery(document).ready(function () {
+  // Mobile Menu
+  $("#mean-menu").meanmenu({
+    // where meanmenu will be placed within the HTML
+    meanMenuContainer: ".mobile-menu",
+
+    // breakpoint
+    meanScreenWidth: "991",
+
+    // set a height here in px, em or %
+    meanNavPush: "",
+
+    // show sub-menus
+    meanShowChildren: true,
+
+    // allow expand/collapse sub-menus
+    meanExpandableChildren: true,
+
+    // expand button
+    meanExpand: "+",
+
+    // collapse button
+    meanContract: "-",
+  });
+
+  // Mobile Menu Sidebar Opend
+  $(".sidebar__toggle-btn").on("click", () => {
+    $(".sidebar__area").addClass("opend");
+  });
+  // Mobile Menu Sidebar Closed
+  $(".sidebar__close-btn").on("click", () => {
+    $(".sidebar__area").removeClass("opend");
+  });
+
   // Banner Slider Activation
   $(".banner__slider").slick({
     infinite: true,
